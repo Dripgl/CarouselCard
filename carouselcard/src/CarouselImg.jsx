067342b38e890853c39690365e69f57dd25eea41
Gabri <gabriele.zito@lc-sw.com>
2024-03-25 19:57:09 +0100

    import carousel

---
+++ carouselcard/src/CarouselImg.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CarouselItem from "./components/CarouselItem";
+
+
+
+function CarouselImg() {
+
+    return(
+        <div>
+            <CarouselItem/>
+        </div>
+    )
+}
